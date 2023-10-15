2d71cd7628da6d71e9a35e11d97793c9ccb0659d
shadowcat143 <jameskuyt655@gmail.com>
2023-10-14 23:34:11 -0400

    meow

--- script.js
+++ script.js
@@ -1,63 +1,28 @@
 const scannedItems = [];
 let totalPrice = 0;
 
-const scanButton = document.getElementById('scan-button');
 const checkoutButton = document.getElementById('checkout-button');
-const barcodeInput = document.getElementById('barcode-input');
-const itemNameInput = document.getElementById('item-name-input');
-
-// Define a map of barcodes to prices (you can fetch this from a database)
-const barcodeToPriceMap = {
-    "123456": 5.00,
-    "789012": 10.00,
-    "456789": 2.50,
-    "987654": 3.00,
-    "321654": 8.00,
-    "741852": 4.50,
-    "369258": 6.00,
-    "852963": 2.00,
-    // Add more barcode-price pairs as needed
-};
-
-scanButton.addEventListener('click', () => {
-    const barcode = barcodeInput.value;
-    const itemName = itemNameInput.value; // Get the item name from the input box
-
-    if (barcode) {
-        const itemPrice = barcodeToPriceMap[barcode];
-        if (itemPrice) {
-            scannedItems.push({ name: itemName, price: itemPrice });
-            totalPrice += itemPrice;
-            updateUI();
-            barcodeInput.value = ''; // Clear the barcode input field
-            itemNameInput.value = ''; // Clear the item name input field
-        } else {
-            alert("Barcode not found. Please check the barcode.");
-        }
-    }
-});
+const customerNameInput = document.getElementById('customer-name-input');
 
 checkoutButton.addEventListener('click', () => {
-    alert(`Total Price: $${totalPrice.toFixed(2)}`);
+    const customerName = customerNameInput.value;
+    alert(`Customer Name: ${customerName}\nTotal Price: $${totalPrice.toFixed(2)}`);
     resetGame();
 });
+
+// Define a function to add items from quick access buttons
+function addQuickAccessItem(itemName, itemPrice) {
+    scannedItems.push({ name: itemName, price: itemPrice });
+    totalPrice += itemPrice;
+    updateUI();
+}
 
 const clearAllButton = document.getElementById('clear-all-button');
 const removeLastOrderButton = document.getElementById('remove-last-order-button');
 
-clearAllButton.addEventListener('click', () => {
-    scannedItems.length = 0;
-    totalPrice = 0;
-    updateUI();
-});
+clearAllButton.addEventListener('click', clearAllItems);
 
-removeLastOrderButton.addEventListener('click', () => {
-    if (scannedItems.length > 0) {
-        const lastItem = scannedItems.pop();
-        totalPrice -= lastItem.price;
-        updateUI();
-    }
-});
+removeLastOrderButton.addEventListener('click', removeLastOrder);
 
 function updateUI() {
     const itemList = document.getElementById('item-list');
@@ -66,8 +31,23 @@ function updateUI() {
     totalElement.textContent = `Total Price: $${totalPrice.toFixed(2)}`;
 }
 
+function clearAllItems() {
+    scannedItems.length = 0;
+    totalPrice = 0;
+    updateUI();
+}
+
+function removeLastOrder() {
+    if (scannedItems.length > 0) {
+        const lastItem = scannedItems.pop();
+        totalPrice -= lastItem.price;
+        updateUI();
+    }
+}
+
 function resetGame() {
     scannedItems.length = 0;
     totalPrice = 0;
+    customerNameInput.value = '';
     updateUI();
 }
