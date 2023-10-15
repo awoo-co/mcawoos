ee3d2d32fdf8669cd1e070422abb08c2b049b868
shadowcat143 <jameskuyt655@gmail.com>
2023-10-14 22:14:46 -0400

    meow

--- script.js
+++ script.js
@@ -1,86 +1,113 @@
-const video = document.getElementById('webcam');
-const totalDisplay = document.getElementById('total');
-const itemList = document.getElementById('item-list');
-const vegButton = document.getElementById('classify-veg');
-const fruitButton = document.getElementById('classify-fruit');
-const beep = document.getElementById('beep');
+const scannedItems = [];
+let totalPrice = 0;
 
-let total = 0;
+const videoElement = document.getElementById('camera-feed');
+const captureButton = document.getElementById('capture-button');
 
-vegButton.addEventListener('click', () => classifyItem('vegetable'));
-fruitButton.addEventListener('click', () => classifyItem('fruit'));
+captureButton.disabled = true;
 
-async function initCamera() {
-    try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-        video.srcObject = stream;
-        await video.play();
-    } catch (error) {
-        console.error("Error accessing the webcam:", error);
+navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+        videoElement.srcObject = stream;
+        captureButton.disabled = false;
+    })
+    .catch(function (error) {
+        console.error('Error accessing camera:', error);
+    });
+
+captureButton.addEventListener('click', function () {
+    const canvas = document.createElement('canvas');
+    canvas.width = videoElement.videoWidth;
+    canvas.height = videoElement.videoHeight;
+    canvas.getContext('2d').drawImage(videoElement, 0, 0, canvas.width, canvas.height);
+
+    // Convert the data URL to a Blob
+    const imageFile = canvas.toDataURL('image/jpeg');
+    const base64Data = imageFile.split(',')[1];
+    const byteCharacters = atob(base64Data);
+    const byteNumbers = new Uint8Array(byteCharacters.length);
+    
+    for (let i = 0; i < byteCharacters.length; i++) {
+        byteNumbers[i] = byteCharacters.charCodeAt(i);
     }
-
-    const codeReader = new ZXing.BrowserQRCodeReader();
+    
+    const blob = new Blob([byteNumbers], { type: 'image/jpeg' });
 
     try {
-        const devices = await codeReader.listVideoInputDevices();
-        if (devices.length > 0) {
-            codeReader.decodeFromVideoDevice(devices[0].deviceId, 'webcam', (result, error) => {
-                if (result) {
-                    const barcode = result.text;
-                    const itemName = getItemNameByBarcode(barcode);
-                    if (itemName) {
-                        addItem(itemName, getItemPrice(itemName));
-                        playBeepSound(); // Play a beep sound
-                    }
+        readBarcodeFromCamera(blob)
+            .then(barcode => {
+                if (barcode) {
+                    // Replace this with a lookup from your barcode database
+                    const itemName = "Item Name"; 
+                    const itemPrice = 10.00; // Replace with the actual price
+                    scannedItems.push({ name: itemName, price: itemPrice });
+                    totalPrice += itemPrice;
+                    updateUI();
                 } else {
-                    console.error("Barcode scanning error:", error);
+                    alert("Barcode not detected.");
                 }
+            })
+            .catch(error => {
+                alert("Error reading barcode: " + error.message);
             });
+    } catch (error) {
+        alert("Error capturing image: " + error.message);
+    }
+});
+
+document.getElementById('checkout-button').addEventListener('click', () => {
+    alert(`Total Price: $${totalPrice.toFixed(2)}`);
+    resetGame();
+});
+
+function updateUI() {
+    const itemList = document.getElementById('item-list');
+    const totalElement = document.getElementById('total-price');
+    itemList.innerHTML = '<ul>' + scannedItems.map(item => `<li>${item.name}: $${item.price.toFixed(2)}</li>`).join('') + '</ul>';
+    totalElement.textContent = `Total Price: $${totalPrice.toFixed(2)}`;
+}
+
+function resetGame() {
+    scannedItems.length = 0;
+    totalPrice = 0;
+    updateUI();
+}
+
+async function readBarcodeFromCamera(imageBlob) {
+    var form = new FormData();
+    form.append("imageFile", imageBlob, "file");
+    
+    var settings = {
+         "url": "https://api.cloudmersive.com/barcode/scan/image",
+         "method": "POST",
+         "timeout": 0,
+         "headers": {
+              "Apikey": "693c8c06-fca9-44f6-bee5-f36cce501ed0"
+         },
+         "processData": false,
+         "mimeType": "multipart/form-data",
+         "contentType": false,
+         "data": form
+    };
+
+    try {
+        const response = await fetch("https://api.cloudmersive.com/barcode/scan/image", settings);
+        const responseText = await response.text();
+        
+        // Check if the response is valid JSON
+        let data;
+        try {
+            data = JSON.parse(responseText);
+        } catch (error) {
+            throw new Error("Invalid JSON response from the API.");
+        }
+
+        if (data && data.Barcode) {
+            return data.Barcode;
         } else {
-            console.error("No video input devices found.");
+            return null;
         }
     } catch (error) {
-        console.error("Error accessing video input devices:", error);
+        throw new Error("Failed to read barcode: " + error.message);
     }
 }
-
-function getItemNameByBarcode(barcode) {
-    // Simulated product database
-    const products = {
-        '1234567890123': 'Product 1',
-        '9876543210987': 'Product 2',
-        // Add more products as needed
-    };
-
-    return products[barcode] || 'Unknown Product';
-}
-
-function getItemPrice(itemName) {
-    // Simulated price lookup
-    const prices = {
-        'Product 1': 2.99,
-        'Product 2': 1.99,
-        // Add more prices as needed
-    };
-
-    return prices[itemName] || 0;
-}
-
-function addItem(itemName, itemPrice) {
-    const li = document.createElement('li');
-    li.innerText = `${itemName} - $${itemPrice.toFixed(2)}`;
-    itemList.appendChild(li);
-    total += itemPrice;
-    totalDisplay.innerText = total.toFixed(2);
-}
-
-function playBeepSound() {
-    beep.play();
-}
-
-function classifyItem(category) {
-    // Add code to classify the currently scanned item as "vegetable" or "fruit"
-    // You can update the UI or perform further actions based on the classification.
-}
-
-initCamera();
